e23788254e08a22dfef4ecf4073610f705708fc2
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-01-26 00:50:58 +0530

    Time: 30 ms (21.52%) | Memory: 47.2 MB (19.57%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int helper(int[] coins,int[][] dp,int amount,int idx){
+        if(idx==0){
+            if(amount%coins[0]==0) return amount/coins[0];
+            return (int)(1e9);
+        }
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        int pick=(int)(1e9);
+        if(coins[idx]<=amount) pick=1+helper(coins,dp,amount-coins[idx],idx);  //keep idx still
+        int notPick=helper(coins,dp,amount,idx-1);
+
+        return dp[idx][amount]=Math.min(pick,notPick);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j]=-1;
+            }
+        }
+        int ans = helper(coins,dp,amount,n-1);
+        if(ans==(int)(1e9)) return -1;
+        return ans;
+    }
+}
