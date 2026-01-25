aeac741615f95b22777d50d9b6fc241f5fd3eec4
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-01-26 01:11:49 +0530

    Time: 16 ms (66.27%) | Memory: 47.4 MB (15.99%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -14,13 +14,28 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp = new int[n][amount+1];
-        for(int i=0;i<n;i++){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=amount;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // int ans = helper(coins,dp,amount,n-1);
+        // if(ans==(int)(1e9)) return -1;
+        // return ans;
+
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0) dp[0][j]=j/coins[0];
+            else dp[0][j]=(int)(1e9);
+        }
+        for(int i=1;i<n;i++){
             for(int j=0;j<=amount;j++){
-                dp[i][j]=-1;
+                int pick=(int)(1e9);
+                if(coins[i]<=j) pick=1+dp[i][j-coins[i]];
+                int notPick=dp[i-1][j];
+                dp[i][j]=Math.min(pick,notPick);
             }
         }
-        int ans = helper(coins,dp,amount,n-1);
-        if(ans==(int)(1e9)) return -1;
-        return ans;
+        if(dp[n-1][amount]==(int)(1e9)) return -1;
+        return dp[n-1][amount];
     }
 }
