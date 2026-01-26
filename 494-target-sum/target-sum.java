888fcb859996e25ac4d3c4efb258962619b2faaa
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-01-26 14:24:27 +0530

    Time: 5 ms (80.09%) | Memory: 45.2 MB (43.19%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -4,7 +4,7 @@ class Solution {
             if(sum==0) return 1;
             return 0;
         }
-        // if(idx==0){
+        // if(idx==0){        //this base case is wrong while handling zeroes
         //     if(sum==nums[0] || sum==0) return 1;
         //     return 0;
         // }
@@ -27,11 +27,27 @@ class Solution {
         int requireTarget=(total-target)/2;
 
         int[][] dp = new int[n][requireTarget+1];
-        for(int i=0;i<n;i++){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=requireTarget;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return helper(nums,dp,requireTarget,n-1);
+
+        //tabulation
+        if(nums[0]==0) dp[0][0]=2;
+        else dp[0][0]=1;
+
+        if(nums[0]!=0 && nums[0]<=requireTarget) dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++){
             for(int j=0;j<=requireTarget;j++){
-                dp[i][j]=-1;
+                // if(nums[i]==0) return 2*dp[i-1][j];
+                int pick=0;
+                if(nums[i]<=j) pick=dp[i-1][j-nums[i]];
+                int notPick=dp[i-1][j];
+                dp[i][j]=pick+notPick;
             }
         }
-        return helper(nums,dp,requireTarget,n-1);
+        return dp[n-1][requireTarget];
     }
 }
