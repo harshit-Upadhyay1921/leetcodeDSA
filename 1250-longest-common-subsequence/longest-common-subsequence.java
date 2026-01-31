1d84e5093e3e1dacf28c1a95f210c162a6d6bd70
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-01-31 14:30:55 +0530

    Time: 22 ms (49.63%) | Memory: 54.2 MB (17.12%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,18 +5,6 @@ class Solution {
         //     if(text1.charAt(i1)==text2.charAt(i2)) return 1;
         //     // return 0;
         // }
-        // if(i1==0 && i2!=0){
-        //     if(text1.charAt(i1)==text2.charAt(i2)){
-        //         return 1+helper(text1,text2,dp,i1,i2-1);
-        //     }
-        //     return helper(text1,text2,dp,i1,i2-1);
-        // }
-        // if(i2==0 && i1!=0){
-        //     if(text1.charAt(i1)==text2.charAt(i2)){
-        //         return 1+helper(text1,text2,dp,i1-1,i2);
-        //     }
-        //     return helper(text1,text2,dp,i1-1,i2);
-        // }
         if(dp[i1][i2]!=-1) return dp[i1][i2];
         int match=-(int)(1e9);
         if(text1.charAt(i1)==text2.charAt(i2)){
@@ -29,12 +17,33 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n1=text1.length();
         int n2=text2.length();
-        int[][] dp = new int[n1][n2];
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                dp[i][j]=-1;
+        // int[][] dp = new int[n1][n2];
+        // for(int i=0;i<n1;i++){
+        //     for(int j=0;j<n2;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return helper(text1,text2,dp,n1-1,n2-1);
+        int[][] dp = new int[n1+1][n2+1];
+        //base case
+        for(int i1=0;i1<=n1;i1++){
+            dp[i1][0]=0;
+        }
+        for(int i2=0;i2<=n2;i2++){
+            dp[0][i2]=0;
+        }
+
+        for(int i1=1;i1<=n1;i1++){
+            for(int i2=1;i2<=n2;i2++){
+                int match=-(int)(1e9);
+                int notMatch=-(int)(1e9);
+                if(text1.charAt(i1-1)==text2.charAt(i2-1)){
+                    match=1+dp[i1-1][i2-1];
+                }
+                else notMatch=Math.max(dp[i1-1][i2],dp[i1][i2-1]);
+                dp[i1][i2]=Math.max(match,notMatch);
             }
         }
-        return helper(text1,text2,dp,n1-1,n2-1);
+        return dp[n1][n2];
     }
 }
