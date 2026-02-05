fce513f583e48e7fd8ca6ba77b594194cda98285
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-02-05 11:16:11 +0530

    Time: 1 ms (98.72%) | Memory: 46.8 MB (51.92%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -2,12 +2,13 @@ class Solution {
     public int[] constructTransformedArray(int[] nums) {
         int n=nums.length;
         int[] result = new int[n];
+        if(n==1) return nums;  //for handling single length test cases like [10],[-10],[-100];
         for(int i=0;i<n;i++){
             if(nums[i]==0) result[i]=nums[i];
             else if(nums[i]>0) result[i]=nums[(i+nums[i])%n];
             else{
                 int idx=i-Math.abs(nums[i]);
-                while(idx<0) idx=n+idx;
+                while(idx<0) idx=n+idx;   //although this handles single length test cases. 
                 result[i]=nums[idx];
             }
         }
