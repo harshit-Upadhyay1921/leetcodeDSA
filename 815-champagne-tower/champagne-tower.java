1720e4bdc81ed8139ad4a5bc8223daf797bb955f
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-02-14 16:36:50 +0530

    Time: 13 ms (6.01%) | Memory: 48.7 MB (9.54%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -11,11 +11,28 @@ class Solution {
     }
     public double champagneTower(int poured, int query_row, int query_glass) {
         double[][] dp = new double[101][101];
-        for(int i=0;i<101;i++){
-            for(int j=0;j<101;j++){
-                dp[i][j]=-1.0;
+        // for(int i=0;i<101;i++){
+        //     for(int j=0;j<101;j++){
+        //         dp[i][j]=-1.0;
+        //     }
+        // }
+        // return Math.min(1.0, helper(poured,query_row,query_glass,dp));
+        dp[0][0]=poured/1.0;
+        for(int i=1;i<101;i++){
+            for(int j=0;j<=i;j++){
+                double leftUp = 0.0;
+                double rightUp = 0.0;
+                if(i-1>=0 && j-1>=0){
+                    leftUp = (dp[i-1][j-1]-1)/2.0;
+                    if(leftUp<0) leftUp = 0.0;
+                }
+                if(i-1>=0){
+                    rightUp = (dp[i-1][j]-1)/2.0;
+                    if(rightUp<0) rightUp = 0.0;
+                }
+                dp[i][j] = leftUp+rightUp;
             }
         }
-        return Math.min(1.0, helper(poured,query_row,query_glass,dp));
+        return Math.min(1.0, dp[query_row][query_glass]);
     }
 }
