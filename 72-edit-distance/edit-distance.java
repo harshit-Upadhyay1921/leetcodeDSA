6e9b7c9988f0974b09bbb09fe29311607bbc2496
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-02-20 06:46:09 +0530

    Time: 3 ms (97.51%) | Memory: 47 MB (87.97%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,10 +1,10 @@
 class Solution {
     public int helper(int i,int j,int[][] dp,String w1,String w2){
-        if(j>=w2.length()){
-            return w1.length()-i;
+        if(j<0){
+            return i+1;
         }
-        if(i>=w1.length()){
-            return w2.length()-j;
+        if(i<0){
+            return j+1;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         int match=0;
@@ -12,11 +12,11 @@ class Solution {
         int notMatchDelete=0;
         int notMatchReplace=0;
         if(w1.charAt(i)==w2.charAt(j)){
-            match=helper(i+1,j+1,dp,w1,w2);
+            match=helper(i-1,j-1,dp,w1,w2);
         }else{
-            notMatchInsert=1+helper(i,j+1,dp,w1,w2);
-            notMatchDelete=1+helper(i+1,j,dp,w1,w2);
-            notMatchReplace=1+helper(i+1,j+1,dp,w1,w2);
+            notMatchInsert=1+helper(i,j-1,dp,w1,w2);
+            notMatchDelete=1+helper(i-1,j,dp,w1,w2);
+            notMatchReplace=1+helper(i-1,j-1,dp,w1,w2);
         }
         return dp[i][j]=match + Math.min(notMatchInsert, Math.min(notMatchDelete, notMatchReplace));
     }
@@ -29,6 +29,6 @@ class Solution {
                 dp[i][j]=-1;
             }
         }
-        return helper(0,0,dp,word1,word2);
+        return helper(n1-1,n2-1,dp,word1,word2);
     }
 }
