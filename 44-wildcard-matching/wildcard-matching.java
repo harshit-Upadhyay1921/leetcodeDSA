d0ed34e25afe4e5dca8bd8c5ecf03105b4b1d36f
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-02-21 10:08:58 +0530

    Time: 15 ms (72.15%) | Memory: 60.5 MB (27.95%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,43 +1,30 @@
 class Solution {
-    public boolean helper(String p,String s,int[][] dp,int i,int j){
+    public boolean helper(String p,String s,Boolean[][] dp,int i,int j){
         if(i<0 && j<0){
             return true;
         }
-        if(i<0 && j>=0) return false;
+        if(i<0) return false;
         if(j<0){
             for(int k=0;k<=i;k++){
                 if(p.charAt(k)!='*') return false;
             }
             return true;
-        } 
-        if(dp[i][j]!=-1) return dp[i][j]==1;
-        //if(p.charAt(i)!='*' && p.charAt(i)!=s.charAt(j)) return false;
-        boolean result=false;
-        if(p.charAt(i)==s.charAt(j) || p.charAt(i)=='?') result = helper(p,s,dp,i-1,j-1);
-        else if(p.charAt(i)=='*'){
-            boolean one = helper(p,s,dp,i,j-1);
-            boolean ans = false;
-            for(int k=0;k<=j+1;k++){
-                if(helper(p,s,dp,i-1,j-k)==true){
-                    ans=true;
-                    break;
-                }
-                
-            }
-            result = ans || one;
         }
-        dp[i][j] = result ? 1:0;
-        return result;
+        if(dp[i][j]!=null) return dp[i][j];
+        boolean match=false;
+        boolean notMatch=false; 
+        if(p.charAt(i)==s.charAt(j) || p.charAt(i)=='?') match=helper(p,s,dp,i-1,j-1);
+        else if(p.charAt(i)=='*'){
+            notMatch=helper(p,s,dp,i-1,j) || helper(p,s,dp,i,j-1);
+        }
+        else if(p.charAt(i)!=s.charAt(j)) return false;
+
+        return dp[i][j] = match || notMatch;
     }
     public boolean isMatch(String s, String p) {
         int n =  p.length();
         int m = s.length();
-        int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
-            }
-        }
+        Boolean[][] dp = new Boolean[n][m];
         return helper(p,s,dp,n-1,m-1);
     }
 }
