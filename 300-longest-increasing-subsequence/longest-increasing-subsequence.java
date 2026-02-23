ac430f37fbf76ba42626e9e1130432c5ec4f6a9d
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-02-23 18:55:29 +0530

    Time: 88 ms (22.28%) | Memory: 237.1 MB (16.58%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -13,12 +13,25 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         // int lastPicked = -(int)(1e9);  //can not memoize using element have to use index.
         int n=nums.length;
-        int[][] dp = new int[n][n+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n+1;j++){
-                dp[i][j]=-1;
+        // int[][] dp = new int[n][n+1];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n+1;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return helper(nums,dp,0,-1);
+
+        //tabulation
+        int[][] dp = new int[n+1][n+1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int pick = -(int)(1e9);
+                if(j==-1 || nums[i]>nums[j]) pick = 1+dp[i+1][i+1];
+                int notPick = dp[i+1][j+1];
+
+                dp[i][j+1]=Math.max(pick,notPick);
             }
         }
-        return helper(nums,dp,0,-1);
+        return dp[0][0];
     }
 }
