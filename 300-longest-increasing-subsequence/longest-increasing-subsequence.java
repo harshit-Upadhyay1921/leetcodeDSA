5045c84e7e52acbbe36bc417b0c6a2efcee2578e
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-02-27 00:19:46 +0530

    Time: 7 ms (82.33%) | Memory: 46.4 MB (42.28%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,20 +1,30 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,1);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i] = Math.max(dp[i],1+dp[j]);
-                }
+    public int binarySearch(List<Integer> list,int el){
+        int l=0;int r=list.size()-1;  //1 4 5 9
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(list.get(mid)==el) return mid;
+            else if(list.get(mid)<el) l=mid+1;
+            else{
+                ans=mid;
+                r=mid-1;
             }
         }
-        int max = -1;
-        for(int el:dp){
-            max=Math.max(max,el);
+        return ans;
+    }
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int el = nums[i];
+            if(list.isEmpty() || list.get(list.size()-1)<el){
+                list.add(el);
+            }else{
+                int idx = binarySearch(list,el);
+                list.set(idx,el);
+            }
         }
-        return max;
+        return list.size();
     }
 }
