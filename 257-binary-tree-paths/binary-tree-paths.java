eecb9bdeebf90315aa2ab39771f50aefc2403770
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-03-15 00:13:18 +0530

    Time: 1 ms (99.83%) | Memory: 43.7 MB (98.29%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void helper(TreeNode root,StringBuilder sb,List<String> list){
+        if(root==null) return;
+        int len = sb.length();//saving original length;
+        sb.append(root.val);
+        if(root.left==null && root.right==null){
+            list.add(sb.toString());
+        }else{
+            sb.append("->");
+            helper(root.left,sb,list);
+            helper(root.right,sb,list);
+        }
+        sb.setLength(len);//restoring original length so it will auto remove
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(root,sb,list);
+        return list;
+    }
+}
