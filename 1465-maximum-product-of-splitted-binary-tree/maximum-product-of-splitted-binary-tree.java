4f877a861a766cce5f12aff8fea88ec961ef95d3
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-03-18 12:23:23 +0530

    Time: 6 ms (79.87%) | Memory: 66 MB (10.27%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -1,11 +1,11 @@
 
 class Solution {
-    long max=1;
+    // long max=1;
     long mod=1000000007;
-    public long helper(long totalSum,TreeNode root){
+    public long helper(long totalSum,TreeNode root,long[] max){
         if(root==null) return 0;
-        long curr = (long)root.val+helper(totalSum,root.left)+helper(totalSum,root.right);
-        max = Math.max(max,curr*(totalSum-curr));
+        long curr = (long)root.val+helper(totalSum,root.left,max)+helper(totalSum,root.right,max);
+        max[0] = Math.max(max[0],curr*(totalSum-curr));
         return curr;
     }
     public long dfs(TreeNode root){
@@ -14,7 +14,9 @@ class Solution {
     }
     public int maxProduct(TreeNode root) {
         long totalSum = dfs(root);
-        helper(totalSum,root);
-        return (int)(max%mod);
+        long[] max = new long[1];
+        max[0]=1;
+        helper(totalSum,root,max);
+        return (int)(max[0]%mod);
     }
 }
