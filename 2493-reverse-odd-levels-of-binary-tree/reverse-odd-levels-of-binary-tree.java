78fcff85db26218f825f70a5507d1b21ce72da5a
Harshit Upadhyay <harshitupadhyay1921@gmail.com>
2026-03-18 12:37:19 +0530

    Time: 1 ms (92.93%) | Memory: 49.5 MB (77.95%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,29 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    private void helper(TreeNode leftNode,TreeNode rightNode,int level){
-        if(leftNode==null && rightNode==null) return;
+    public void helper(TreeNode leftTree,TreeNode rightTree,int level){
+        if(leftTree==null || rightTree==null) return;
         if(level%2!=0){
-            int temp=leftNode.val;
-            leftNode.val=rightNode.val;
-            rightNode.val=temp;
+            int temp=leftTree.val;
+            leftTree.val=rightTree.val;
+            rightTree.val=temp;
         }
-        helper(leftNode.left,rightNode.right,level+1);
-        helper(leftNode.right,rightNode.left,level+1);
-        return;
+        helper(leftTree.left,rightTree.right,level+1);
+        helper(leftTree.right,rightTree.left,level+1);
     }
     public TreeNode reverseOddLevels(TreeNode root) {
         helper(root.left,root.right,1);
